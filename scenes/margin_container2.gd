c9d539af39746be24128ccec23f81b611444e7c5
Brian Taylor <brian@ipr.com>
2026-08-17 17:50:17 +0200

    Game menu
    Created Game menu. Mostly spend time furious with button sizes. Graphics still need to added, but at least it does what it is supposed to do. Spider is a big help. Although, his insistance with adding another level is strange. But, maybe it is better to acount for games expansion, if 3 levels are not enought... I hope they are... Being scared all the time is exhausting... and makes it hard to think...

---
+++ scenes/margin_container2.gd
@@ -0,0 +1,37 @@
+extends MarginContainer
+var time = Time.get_date_string_from_system()
+var time1 = "2026-08-16"
+var time2 = "2026-08-17"
+var time3 = "2026-08-18"
+var time4 = "2026-12-18"
+
+@onready var tutorial: Button = $VBoxContainer/Tutorial
+@onready var level_1: Button = $"VBoxContainer/Level 1"
+@onready var level_2: Button = $"VBoxContainer/Level 2"
+@onready var level_3: Button = $"VBoxContainer/Level 3"
+@onready var level_4: Button = $"VBoxContainer/Level 4"
+@onready var exit: Button = $VBoxContainer/Exit
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	tutorial.text = "Tutorial"
+	button_time(level_1, time1, "Paranoia", "I. House that was not there")	
+	button_time(level_2, time2, "Acknowlegment", "II. to Know")	
+	button_time(level_3, time3, "Isolation", "III. Through the Fog")	
+	button_time(level_4, time4, "Nudging", "IV. ???")	
+	exit.text = "Exit"
+
+func button_time(button, timeTo, text1, text2):
+	if(time>=timeTo):
+		button.text = text2
+	else:
+		button.text = text1
+		button.disabled = true
+
+
+
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn") # Replace with function body.
+
+	get_tree().quit()
